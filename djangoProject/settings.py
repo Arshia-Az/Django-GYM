563e93174ceda0f1c08ac1268f08842c905e497a
arshia <arshiagt113@gmail.com>
2024-10-31 06:29:57 +0330

    add static file and create home view

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #external app
+    'django_render_partial',
+
+    #internal app
+    'home_module',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

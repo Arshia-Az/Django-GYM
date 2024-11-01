d70b77551358c95f2ffb3d9d06fc0a5c8a7d248c
arshia <arshiagt113@gmail.com>
2024-11-01 06:16:53 +0330

    create block body and file templates/home/index.html and change index.html to the base.html

--- home_module/views.py
+++ home_module/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "base/index.html")
+    return render(request, "home/index.html")
 
 
 def header_component(request):
